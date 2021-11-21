b238c0631d7b54b4c49c8717196782b2244c5055
Stanislau Shlyahtich <stanislau.shlyahtich@sprintingsoftware.com>
2021-11-21 19:51:41 +0300

    Fix sort import

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import { initMocks } from './test/server';
-
 // initMocks();
 
 ReactDOM.render(
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,16 @@
 import { Navigate, useRoutes } from 'react-router-dom';
 
-// import { Landing } from '@/features/misc';
 import { protectedRoutes } from './protected';
 import { publicRoutes } from './public';
 
-import { Dashboard } from '@/features/misc';
+// import { Dashboard } from '@/features/misc';
 import { useAuth } from '@/lib/auth';
 
 export const AppRoutes = () => {
   const auth = useAuth();
 
   // const commonRoutes = [{ path: '/', element: <Landing /> }];
-  const elements = auth.user ? <Dashboard /> : <Navigate to={'/auth/login'} />;
+  const elements = auth.user ? <Navigate to={'/app'} /> : <Navigate to={'/auth/login'} />;
   const commonRoutes = [{ path: '/', element: elements }];
 
   const routes = auth.user ? protectedRoutes : publicRoutes;
